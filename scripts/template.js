6035a14fe4121d8355c59e6a665de28218651f44
orangina75 <post.rux@gmail.com>
2024-12-01 10:40:50 +0100

    update contacts

--- scripts/template.js
+++ scripts/template.js
@@ -2,13 +2,15 @@
 
 function getContactsTemplate(contact, initials) {
     return `
-        <div class="contact-header">
+        <div class="contact-info">
             <div class="contact-initials-circle">
                 <span>${initials}</span>
             </div>
-            <h3>${contact.name}</h3>
+            <div class="contact-name-and-email">
+                <h3>${contact.name}</h3>
+                <p>${contact.email}</p>
+            </div>
         </div>
-        <p>${contact.email}</p>
     `;
 }
 
