c79aee8294b50322cf216b5ecbc66ef63639d4fa
orangina75 <post.rux@gmail.com>
2024-12-03 13:33:55 +0100

    update contacts

--- scripts/contacts.js
+++ scripts/contacts.js
@@ -50,6 +50,20 @@ function getLetterDivider(letter) {
     return { letterElement, dividerElement };
 }
 
+function getContactsTemplate(contact, initials) {
+    return `
+        <div class="contact-info">
+            <div class="contact-initials-circle">
+                <span>${initials}</span>
+            </div>
+            <div class="contact-name-and-email">
+                <h3>${contact.name}</h3>
+                <p>${contact.email}</p>
+            </div>
+        </div>
+    `;
+}
+
 function createContactElement(contact, initials) {
     let contactElement = document.createElement("div");
     contactElement.classList.add("contact-item");
