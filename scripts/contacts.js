0093241e1efc6f4ad1b59c99bbba63225fe6a1eb
orangina75 <post.rux@gmail.com>
2024-12-11 21:53:49 +0100

    update contacts

--- scripts/contacts.js
+++ scripts/contacts.js
@@ -242,8 +242,11 @@ function createAddContactCard(contact) {
                     alt="Logo Contact Phone">
                 </div>
                 <div class="add-contact-buttons">
-                    <button class="cancel-contact-button"><h2>Cancel</h2>
-                        <img class="cancel-contact-icon" src="../../assets/icons/contact_cancel.png" alt="Icon Cancel Contact">
+                    <button class="cancel-contact-button">
+                        <h2>Cancel</h2>
+                        <svg class="cancel-contact-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="25" viewBox="0 0 24 25" fill="none">
+                            <path d="M12.001 12.5001L17.244 17.7431M6.758 17.7431L12.001 12.5001L6.758 17.7431ZM17.244 7.25708L12 12.5001L17.244 7.25708ZM12 12.5001L6.758 7.25708L12 12.5001Z" stroke="#2A3647" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
                     </button>
                     <button class="save-contact-button"><h2>Create contact</h2>
                         <img class="create-contact-icon" src="../../assets/icons/contact_create.png" alt="Icon Create Contact">
@@ -253,6 +256,9 @@ function createAddContactCard(contact) {
     `;
     let saveButton = addContactCard.querySelector(".save-contact-button");
     saveButton.addEventListener("click", saveNewContact);
+    let cancelButton = addContactCard.querySelector(".cancel-contact-button");
+    cancelButton.addEventListener("click", closeModalContact);
+    
     return addContactCard;
 }
 
