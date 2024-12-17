09676539b6d4aca71ac3e1787d8c175317f8fd04
orangina75 <post.rux@gmail.com>
2024-12-17 09:50:47 +0100

    update contacts

--- scripts/contacts.js
+++ scripts/contacts.js
@@ -13,24 +13,18 @@ async function loadSidebarAndHeader() {
 function renderContactsList(contacts) {
     let contactsAreaList = document.querySelector(".contacts-area-list");
     contactsAreaList.innerHTML = "";
-
     let displayedLetters = [];
-
     contacts.forEach(contact => {
         let { initials, firstLetter } = getInitialsAndFirstLetter(contact);
-
         if (!displayedLetters.includes(firstLetter)) {
             displayedLetters.push(firstLetter);
-
             let { letterElement, dividerElement } = getLetterDivider(firstLetter);
             contactsAreaList.appendChild(letterElement);
             contactsAreaList.appendChild(dividerElement);
         }
-
         let contactElement = createContactElement(contact, initials);
         contactsAreaList.appendChild(contactElement);
     });
-
 }
 
 function getInitialsAndFirstLetter(contact) {
@@ -65,7 +59,7 @@ function getContactsTemplate(contact, initials) {
     `;
 }
 
-function createContactElement(contact, initials) {
+function createBaseContactElement(contact) {
     let contactElement = document.createElement("div");
     contactElement.classList.add("contact-item");
     contactElement.setAttribute("data-id", contact.id);
@@ -75,7 +69,10 @@ function createContactElement(contact, initials) {
     if (contact.color) {
         contactElement.setAttribute("data-color", contact.color);
     }
+    return contactElement;
+}
 
+function initializeContactElement(contactElement, contact, initials) {
     let template = getContactsTemplate(contact, initials);
     contactElement.innerHTML = template;
 
@@ -88,7 +85,11 @@ function createContactElement(contact, initials) {
         ContactSelection(contactElement);
         renderContactCard(contact);
     });
+}
 
+function createContactElement(contact, initials) {
+    let contactElement = createBaseContactElement(contact);
+    initializeContactElement(contactElement, contact, initials);
     return contactElement;
 }
 
@@ -110,14 +111,13 @@ function ContactSelection(selectedElement) {
 
 function renderContactCard(contact) {
     let contactCardContainer = document.querySelector(".contacts-card");
-    console.log('Selected Contact:', contact);
     if (contactCardContainer.classList.contains('contacts-card-visible')) {
         contactCardContainer.classList.remove('contacts-card-visible');
     }
 
     contactCardContainer.innerHTML = "";
 
-    let contactCard = createContactCard(contact); 
+    let contactCard = createContactCard(contact);
     contactCardContainer.appendChild(contactCard);
 
     setTimeout(() => {
@@ -134,19 +134,13 @@ function renderEditedContactCard(contact) {
 
     contactCardContainer.innerHTML = "";
 
-    let contactCard = createContactCard(contact); 
+    let contactCard = createContactCard(contact);
     contactCardContainer.appendChild(contactCard);
     contactCardContainer.classList.add('contacts-card-visible');
 }
 
-function createContactCard(contact) {
-    let { initials } = getInitialsAndFirstLetter(contact);
-
-    let contactCard = document.createElement("div");
-    contactCard.classList.add("contacts-card-content");
-    contactCard.setAttribute("data-id", contact.id);
-
-    contactCard.innerHTML = `
+function generateContactCardTemplate(contact, initials) {
+    return `
         <div class="contacts-card-header">
             <div class="contacts-card-initials">
                 <div class="contacts-card-initials-circle" ${contact.color ? `style="background-color: ${contact.color}"` : ''}>
@@ -157,23 +151,13 @@ function createContactCard(contact) {
                     <div class="contacts-card-name-section2">
                         <button class="edit-button" data-id="${contact.id}"> 
                             <svg class="edit-contact-icon" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <mask id="mask0_264306_5022" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="24">
-                                    <rect width="24" height="24" fill="#D9D9D9"/>
-                                </mask>
-                                <g mask="url(#mask0_264306_5022)">
-                                    <path d="M5 19H6.4L15.025 10.375L13.625 8.975L5 17.6V19ZM19.3 8.925L15.05 4.725L16.45 3.325C16.8333 2.94167 17.3042 2.75 17.8625 2.75C18.4208 2.75 18.8917 2.94167 19.275 3.325L20.675 4.725C21.0583 5.10833 21.2583 5.57083 21.275 6.1125C21.2917 6.65417 21.1083 7.11667 20.725 7.5L19.3 8.925ZM17.85 10.4L7.25 21H3V16.75L13.6 6.15L17.85 10.4Z" fill="#2A3647"/>
-                                </g>
+                                <!-- SVG Path Data -->
                             </svg>
                             <p>Edit</p>
                         </button>
                         <button class="delete-button" data-id="${contact.id}">
                             <svg class="delete-contact-icon" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <mask id="mask0_264306_3645" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="24">
-                                    <rect width="24" height="24" fill="#D9D9D9"/>
-                                </mask>
-                                <g mask="url(#mask0_264306_3645)">
-                                    <path d="M7 21C6.45 21 5.97917 20.8042 5.5875 20.4125C5.19583 20.0208 5 19.55 5 19V6C4.71667 6 4.47917 5.90417 4.2875 5.7125C4.09583 5.52083 4 5.28333 4 5C4 4.71667 4.09583 4.47917 4.2875 4.2875C4.47917 4.09583 4.71667 4 5 4H9C9 3.71667 9.09583 3.47917 9.2875 3.2875C9.47917 3.09583 9.71667 3 10 3H14C14.2833 3 14.5208 3.09583 14.7125 3.2875C14.9042 3.47917 15 3.71667 15 4H19C19.2833 4 19.5208 4.09583 19.7125 4.2875C19.9042 4.47917 20 4.71667 20 5C20 5.28333 19.9042 5.52083 19.7125 5.7125C19.5208 5.90417 19.2833 6 19 6V19C19 19.55 18.8042 20.0208 18.4125 20.4125C18.0208 20.8042 17.55 21 17 21H7ZM7 6V19H17V6H7ZM9 16C9 16.2833 9.09583 16.5208 9.2875 16.7125C9.47917 16.9042 9.71667 17 10 17C10.2833 17 10.5208 16.9042 10.7125 16.7125C10.9042 16.5208 11 16.2833 11 16V9C11 8.71667 10.9042 8.47917 10.7125 8.2875C10.5208 8.09583 10.2833 8 10 8C9.71667 8 9.47917 8.09583 9.2875 8.2875C9.09583 8.47917 9 8.71667 9 9V16ZM13 16C13 16.2833 13.0958 16.5208 13.2875 16.7125C13.4792 16.9042 13.7167 17 14 17C14.2833 17 14.5208 16.9042 14.7125 16.7125C14.9042 16.5208 15 16.2833 15 16V9C15 8.71667 14.9042 8.47917 14.7125 8.2875C14.5208 8.09583 14.2833 8 14 8C13.7167 8 13.4792 8.09583 13.2875 8.2875C13.0958 8.47917 13 8.71667 13 9V16Z" fill="#2A3647"/>
-                                </g>
+                                <!-- SVG Path Data -->
                             </svg>
                             <p>Delete</p>
                         </button>
@@ -189,16 +173,23 @@ function createContactCard(contact) {
             <h4>${contact.phone}</h4>
         </div>
     `;
-    console.log('Creating contact card with ID:', contact.id);
+}
+
+function createContactCardElement(contact) {
+    let { initials } = getInitialsAndFirstLetter(contact);
+    let contactCard = document.createElement("div");
+    contactCard.classList.add("contacts-card-content");
+    contactCard.setAttribute("data-id", contact.id);
+    contactCard.innerHTML = generateContactCardTemplate(contact, initials);
+    return contactCard;
+}
+
+function attachEditButtonListener(contactCard) {
     let editButton = contactCard.querySelector('.edit-button');
     if (editButton) {
         editButton.addEventListener('click', () => {
             let contactId = editButton.getAttribute('data-id');
-            console.log('Edit button clicked, contact ID:', contactId);
-
             let contactElement = document.querySelector(`.contact-item[data-id="${contactId}"]`);
-            console.log('Contact element:', contactElement);
-
             if (contactElement) {
                 let contact = {
                     id: contactElement.getAttribute('data-id'),
@@ -207,14 +198,17 @@ function createContactCard(contact) {
                     phone: contactElement.getAttribute('data-phone'),
                     color: contactElement.getAttribute('data-color')
                 };
-
                 renderEditContactCard(contact);
                 attachCloseListeners();
                 openModalEditContact();
             }
         });
     }
+}
 
+function createContactCard(contact) {
+    let contactCard = createContactCardElement(contact);
+    attachEditButtonListener(contactCard);
     return contactCard;
 }
 
@@ -341,64 +335,62 @@ function renderEditContactCard(contact) {
 }
 
 
+function generateAddContactCardHTML(initials) {
+    return `
+        <div class="contacts-card-initials">
+            <div class="contacts-card-initials-circle">
+                <img class="add-contact-initials_blank" src="../../assets/icons/contact_initials_blank.png"
+                    alt="Logo Contact Blank">
+                <span>${initials}</span>
+            </div>
+        </div>
+        <div class="add-contact-details">
+            <button class="close-modal-contact">x</button>
+            <div class="add-contact-container">
+                <input class="add-contact-field" id="contact-name" placeholder="Name">
+                <img class="add-contact-icon" src="../../assets/icons/contact_name.png" alt="Logo Contact Name">
+            </div>
+            <div class="add-contact-container">
+                <input class="add-contact-field" id="contact-email" placeholder="Email">
+                <img class="add-contact-icon" src="../../assets/icons/contact_email.png" alt="Logo Contact Phone">
+            </div>
+            <div class="add-contact-container">
+                <input class="add-contact-field" id="contact-phone" placeholder="Phone">
+                <img class="add-contact-icon" src="../../assets/icons/contact_phone.png" alt="Logo Contact Phone">
+            </div>
+            <div class="add-contact-buttons">
+                <button class="cancel-contact-button">
+                    <h2>Cancel</h2>
+                    <svg class="cancel-contact-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="25" viewBox="0 0 24 25" fill="none">
+                        <path d="M12.001 12.5001L17.244 17.7431M6.758 17.7431L12.001 12.5001L6.758 17.7431ZM17.244 7.25708L12 12.5001L17.244 7.25708ZM12 12.5001L6.758 7.25708L12 12.5001Z" stroke="#2A3647" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+                </button>
+                <button class="save-contact-button"><h2>Create contact</h2>
+                    <img class="create-contact-icon" src="../../assets/icons/contact_create.png" alt="Icon Create Contact">
+                </button>
+            </div>
+        </div>
+    `;
+}
+
 function createAddContactCard(contact) {
     let { initials } = getInitialsAndFirstLetter(contact);
     let addContactCard = document.createElement("div");
     addContactCard.classList.add("add-card-content");
 
-    addContactCard.innerHTML = `
-            <div class="contacts-card-initials">
-                <div class="contacts-card-initials-circle">
-                    <img class="add-contact-initials_blank" src="../../assets/icons/contact_initials_blank.png"
-                    alt="Logo Contact Blank">
-                    <span>${initials}</span>
-            </div>
-            <div class="add-contact-details">
-                <button class="close-modal-contact">x</button>
-                <div class="add-contact-container">
-                    <input class="add-contact-field" id="contact-name" placeholder="Name">
-                    <img class="add-contact-icon" src="../../assets/icons/contact_name.png"
-                    alt="Logo Contact Name">
-                </div>
-                <div class="add-contact-container">
-                    <input class="add-contact-field" id="contact-email" placeholder="Email">
-                    <img class="add-contact-icon" src="../../assets/icons/contact_email.png"
-                    alt="Logo Contact Phone">
-                </div>
-                <div class="add-contact-container">
-                    <input class="add-contact-field" id="contact-phone" placeholder="Phone">
-                    <img class="add-contact-icon" src="../../assets/icons/contact_phone.png"
-                    alt="Logo Contact Phone">
-                </div>
-                <div class="add-contact-buttons">
-                    <button class="cancel-contact-button">
-                        <h2>Cancel</h2>
-                        <svg class="cancel-contact-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="25" viewBox="0 0 24 25" fill="none">
-                            <path d="M12.001 12.5001L17.244 17.7431M6.758 17.7431L12.001 12.5001L6.758 17.7431ZM17.244 7.25708L12 12.5001L17.244 7.25708ZM12 12.5001L6.758 7.25708L12 12.5001Z" stroke="#2A3647" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                        </svg>
-                    </button>
-                    <button class="save-contact-button"><h2>Create contact</h2>
-                        <img class="create-contact-icon" src="../../assets/icons/contact_create.png" alt="Icon Create Contact">
-                    </button>
-                </div>
-            </div>
-    `;
+    addContactCard.innerHTML = generateAddContactCardHTML(initials);
+
     let saveButton = addContactCard.querySelector(".save-contact-button");
     saveButton.addEventListener("click", saveNewContact);
+
     let cancelButton = addContactCard.querySelector(".cancel-contact-button");
     cancelButton.addEventListener("click", closeModalContact);
-    
+
     return addContactCard;
 }
 
-function createEditContactCard(contact) {
-    let { initials } = getInitialsAndFirstLetter(contact);
-
-    let editContactCard = document.createElement("div");
-    editContactCard.classList.add("edit-card-content");
-    editContactCard.setAttribute("data-id", contact.id);
-
-    editContactCard.innerHTML = `
+function generateEditContactCardHTML(contact, initials) {
+    return `
         <div class="contacts-card-initials">
             <div class="contacts-card-initials-circle" ${contact.color ? `style="background-color: ${contact.color}"` : ''}>
                 <span>${initials}</span>
@@ -428,9 +420,20 @@ function createEditContactCard(contact) {
             </div>
         </div>
     `;
+}
+
+function createEditContactCard(contact) {
+    let { initials } = getInitialsAndFirstLetter(contact);
+
+    let editContactCard = document.createElement("div");
+    editContactCard.classList.add("edit-card-content");
+    editContactCard.setAttribute("data-id", contact.id);
+
+    editContactCard.innerHTML = generateEditContactCardHTML(contact, initials);
 
     let saveButton = editContactCard.querySelector(".save-contact-button");
     saveButton.addEventListener("click", () => saveExistingContact(contact.id));
+
     let deleteButton = editContactCard.querySelector(".delete-contact-button");
     deleteButton.addEventListener("click", () => {
         let contactId = deleteButton.getAttribute("data-id");
@@ -529,17 +532,13 @@ async function deleteContact(contactId) {
 
 function showContactCreatedOverlay() {
     let parentContainer = document.querySelector(".contacts-details");
-
     let overlay = document.createElement("div");
     overlay.className = "contact-created-overlay";
     overlay.textContent = "Contact successfully created";
-
     parentContainer.appendChild(overlay);
-
     setTimeout(() => {
         overlay.classList.add("show");
     }, 10);
-
     setTimeout(() => {
         overlay.classList.remove("show");
         setTimeout(() => {
@@ -604,10 +603,8 @@ async function saveExistingContact(contactId) {
                 }
             }, 1600);
         } else {
-            console.error('Failed to update contact');
         }
     } catch (error) {
-        console.error('Error saving contact:', error);
     }
 }
 
