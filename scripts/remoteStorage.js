74daed5ab4db22e9182bb85aedae7f78785aae2d
orangina75 <post.rux@gmail.com>
2024-12-19 18:45:56 +0100

    update addtask

--- scripts/remoteStorage.js
+++ scripts/remoteStorage.js
@@ -1,4 +1,4 @@
-const BASE_URL="https://join-56225-default-rtdb.europe-west1.firebasedatabase.app/";
+// const BASE_URL="https://join-56225-default-rtdb.europe-west1.firebasedatabase.app/";
 
 async function loadContacts() {
     try {
